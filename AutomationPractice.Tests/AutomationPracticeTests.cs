1d8288e374b6a2ad694814c1ee0272d554186c65
NkosinathiM <NkosinathiM@slingshotstudios.com>
2022-08-06 14:35:40 +0200

    added multiple search test

--- AutomationPractice.Tests/AutomationPracticeTests.cs
+++ AutomationPractice.Tests/AutomationPracticeTests.cs
@@ -13,7 +13,18 @@ public class AutomationPracticeTests : BaseTest
         Assert.IsTrue(await _homePage.IsSearchedItemShown("Blouse"));
     }
 
+    [Test]
+    public async Task Search_MultipleSearch_VerifySearchedItemIsInTheSearchResults()
+    {
+        string searchItem = $"Faded Short Sleeve T-shirts,Blouse,Printed Chiffon Dress";
+        var searchProducts= searchItem.Split(',');
 
+        foreach (var searchProduct in searchProducts)
+        {
+            await _homePage.Search(searchProduct);
+            Assert.IsTrue(await _homePage.IsSearchedItemShown(searchProduct));
+        }
+    }
     public AutomationPracticeTests(Browsers browser) : base(browser)
     {
     }
