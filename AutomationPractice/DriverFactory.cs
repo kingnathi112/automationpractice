5aa1b87026a6d91a8086eeab1d7fa593b32b0217
NkosinathiM <NkosinathiM@slingshotstudios.com>
2022-08-06 11:22:50 +0200

    Added Build function to build browser/ driver instance

--- AutomationPractice/DriverFactory.cs
+++ AutomationPractice/DriverFactory.cs
@@ -1,5 +1,91 @@
-﻿namespace AutomationPractice;
+﻿using Microsoft.Playwright;
 
-public class DriverFactory
-{
-}
+namespace AutomationPractice;
+
+    public static class DriverFactory
+    {
+        public async static Task<IPage> Build(Browsers browser, bool isHeadless = false)
+        {
+            switch (browser)
+            {
+                case Browsers.Chrome:
+                {
+                    var _playwright = await Playwright.CreateAsync();
+                    var browserOption = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                    {
+                        Headless = isHeadless,
+                        Channel = "chrome"
+                    });
+                    var context = await browserOption.NewContextAsync( new BrowserNewContextOptions
+                    {
+                        ViewportSize = new ViewportSize
+                        {
+                            Width = 1080,
+                            Height = 720
+                        }
+                    });
+                    return await context.NewPageAsync();
+                    break;
+                }
+                case Browsers.Firefox:
+                {
+                    var _playwright = await Playwright.CreateAsync();
+                    var browserOption = await _playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+                    {
+                        Headless = isHeadless
+                    });
+                    var context = await browserOption.NewContextAsync( new BrowserNewContextOptions
+                    {
+                        ViewportSize = new ViewportSize
+                        {
+                            Width = 1080,
+                            Height = 720
+                        }
+                    });
+                    return await context.NewPageAsync();
+                    break;
+                }
+                case Browsers.Webkit:
+                {
+                    var _playwright = await Playwright.CreateAsync();
+                    var browserOption = await _playwright.Webkit.LaunchAsync(new BrowserTypeLaunchOptions
+                    {
+                        Headless = isHeadless
+                    });
+                    var context = await browserOption.NewContextAsync( new BrowserNewContextOptions
+                    {
+                        ViewportSize = new ViewportSize
+                        {
+                            Width = 1080,
+                            Height = 720
+                        }
+                    });
+                    return await context.NewPageAsync();
+                    break;
+                }
+                case Browsers.Edge:
+                {
+                    var _playwright = await Playwright.CreateAsync();
+                    var browserOption = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                    {
+                        Headless = isHeadless,
+                        Channel = "msedge"
+                    });
+                    var context = await browserOption.NewContextAsync( new BrowserNewContextOptions
+                    {
+                        ViewportSize = new ViewportSize
+                        {
+                            Width = 1080,
+                            Height = 720
+                        }
+                    });
+                    return await context.NewPageAsync();
+                    break;
+                }
+                default:
+                    return null;
+            }
+
+        }
+
+    }
